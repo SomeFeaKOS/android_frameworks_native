8dc9b097ae6789d15cfe46fdbb829b37be4f06ef
Pullakavi Srinivas <spullaka@codeaurora.org>
2018-05-30 12:42:56 +0530

    sf: Add support for multiple displays
    - Extend hotplug handling for more multiple display.
    - Wait for client to connection before hotplug notification.
    
    CRs-Fixed: 2274419
    Change-Id: Ibe355a230480a6ec44bb04c2e451fc8ddca7a0fc

--- services/surfaceflinger/EventThread.cpp
+++ services/surfaceflinger/EventThread.cpp
@@ -231,7 +231,9 @@ Vector<sp<EventThread::Connection> > EventThread::waitForEventLocked(
             }
         }
 
-        if (!timestamp) {
+        // find out connections waiting for events
+        size_t count = mDisplayEventConnections.size();
+        if (!timestamp && count) {
             // no vsync event, see if there are some other event
             eventPending = !mPendingEvents.isEmpty();
             if (eventPending) {
@@ -241,8 +243,6 @@ Vector<sp<EventThread::Connection> > EventThread::waitForEventLocked(
             }
         }
 
-        // find out connections waiting for events
-        size_t count = mDisplayEventConnections.size();
         for (size_t i = 0; i < count;) {
             sp<Connection> connection(mDisplayEventConnections[i].promote());
             if (connection != nullptr) {
